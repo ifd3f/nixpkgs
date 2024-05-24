1bb5f29b4f8df0752d62ac28ef055c6f80016b4a
Astrid Yu <astrid@astrid.tech>
2024-05-24 02:30:15 -0700

    caligula: 0.4.6 -> 0.4.7

--- pkgs/by-name/ca/caligula/package.nix
+++ pkgs/by-name/ca/caligula/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "caligula";
-  version = "0.4.6";
+  version = "0.4.7";
 
   src = fetchFromGitHub {
     owner = "ifd3f";
     repo = "caligula";
     rev = "v${version}";
-    hash = "sha256-nLt+PDPdW7oEMoWqW0iO4nXGlwk7UymWShn0azQt2ro=";
+    hash = "";
   };
 
-  cargoHash = "sha256-8K3twPL7lNUmUUjD+nKATGgcjgoCuFO+bvlujVySXj0=";
+  cargoHash = "";
 
   buildInputs = lib.optionals stdenv.isDarwin (
     with darwin.apple_sdk.frameworks; [
@@ -35,8 +35,6 @@ rustPlatform.buildRustPackage rec {
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ ifd3f sodiboo ];
     platforms = platforms.linux ++ platforms.darwin;
-    # https://github.com/ifd3f/caligula/issues/105
-    broken = stdenv.hostPlatform.isDarwin;
     mainProgram = "caligula";
   };
 }
